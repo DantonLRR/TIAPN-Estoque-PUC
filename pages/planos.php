bc683e7de462630a358d085370d7dbe35b44542c
Lucas Ramos <85021597+spookjin@users.noreply.github.com>
2025-11-02 20:46:11 -0300

    adicao dos planos mudanca na nav bar

---
+++ pages/planos.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estoque Ideal - Planos</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/planos.css" ">
+    <link rel="stylesheet" href="../css/style_Geral.css">
+</head>
+
+<body>
+  <?php include('../assets/navbar/navbar.php'); ?>
+
+  <div class ="container-back mt-5 ">
+
+  </div>
+
+  <div class="container my-5">
+    <h1 class="text-center mb-4">Escolha o plano ideal para você</h1>
+
+    <div class="row justify-content-center">
+      <!-- Planos -->
+      <div class="col-md-4 mb-4">
+        <div class="card h-100 shadow-sm border-pink">
+          <div class="card-body text-center">
+            <h4 class="card-title text-pink fw-bold">Plano 1</h4>
+            <h2 class="card-price text-pink">R$ 1/mês</h2>
+           <p class="card-text mt-3">
+              papapa
+            </p>
+            <ul class="list-unstyled mb-4">
+              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem perspiciatis labore tempore illum veritatis rerum ex aut repudiandae assumenda, dolorem odit, tempora, porro obcaecati ipsam. Quidem eveniet laudantium explicabo dolorum.</li>
+
+            </ul>
+            <a href="#" class="btn btn-pink w-100">Assinar Agora</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-md-4 mb-4">
+        <div class="card h-100 shadow-sm border-pink">
+          <div class="card-body text-center">
+            <h4 class="card-title text-pink fw-bold">Plano 2</h4>
+            <h2 class="card-price text-pink">R$ 500,00/mês</h2>
+           <p class="card-text mt-3">
+              papapa
+            </p>
+            <ul class="list-unstyled mb-4">
+              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem perspiciatis labore tempore illum veritatis rerum ex aut repudiandae assumenda, dolorem odit, tempora, porro obcaecati ipsam. Quidem eveniet laudantium explicabo dolorum.</li>
+
+            </ul>
+            <a href="#" class="btn btn-pink w-100">Assinar Agora</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-md-4 mb-4">
+        <div class="card h-100 shadow-sm border-pink">
+          <div class="card-body text-center">
+            <h4 class="card-title text-pink fw-bold">Plano 3</h4>
+            <h2 class="card-price text-pink">R$ 1000,00/mês</h2>
+            <p class="card-text mt-3">
+              papapa
+            </p>
+            <ul class="list-unstyled mb-4">
+              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem perspiciatis labore tempore illum veritatis rerum ex aut repudiandae assumenda, dolorem odit, tempora, porro obcaecati ipsam. Quidem eveniet laudantium explicabo dolorum.</li>
+
+            </ul>
+            <a href="#" class="btn btn-pink w-100">Assinar Agora</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
