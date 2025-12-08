f92e7413044118581f1ad9c5f6e7d5c70bbf4280
Marcus Andrade <marcusandrade44@gmail.com>
2025-12-08 15:36:22 -0300

    ReadME

--- banco/banco_tiapn.sql
+++ banco/banco_tiapn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05/12/2025 às 19:04
+-- Tempo de geração: 08/12/2025 às 18:56
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -55,7 +55,8 @@ INSERT INTO `clientes` (`id`, `nome`, `email`, `tipo_cliente`, `cpf`, `cnpj`, `l
 (2, 'Marcus', 'stmarcusandrade44@gmail.com', 'fisica', 1111111, 0, 'Rua Maria Cândida de Almeida', '144', '', 'Diamante (Barreiro)', 'Belo Horizonte', 'MG', '30660-308', 'residencial', '2025-11-29', '2025-11-29'),
 (3, 'Marcus', 'stmarcusandrade44@gmail.com', 'juridica', 0, 2147483647, 'Rua Maria Cândida de Almeida', '144', '', 'Diamante (Barreiro)', 'Belo Horizonte', 'MG', '30660-308', 'comercial', '2025-11-29', '2025-11-29'),
 (4, 'Guilherme', 'ggr@gmail.com', 'fisica', 1345679089, 0, 'Rua Maria Cândida de Almeida', '144', '', 'Diamante (Barreiro)', 'Belo Horizonte', 'MG', '30660-308', 'residencial', '2025-11-29', '2025-11-29'),
-(5, 'Guilherme', 'guilherme44@gmail.com', 'juridica', 0, 2147483647, 'Rua Vereador Orlando Pacheco', '222', '', 'Bairro das Indústrias I (Barreiro)', 'Belo Horizonte', 'MG', '30610-020', 'comercial', '2025-11-30', '2025-11-30');
+(5, 'Guilherme', 'guilherme44@gmail.com', 'juridica', 0, 2147483647, 'Rua Vereador Orlando Pacheco', '222', '', 'Bairro das Indústrias I (Barreiro)', 'Belo Horizonte', 'MG', '30610-020', 'comercial', '2025-11-30', '2025-11-30'),
+(6, 'Guilherme', 'ggr@gmail.com', 'fisica', 1345679089, 0, 'Rua Vereador Orlando Pacheco', '144', '', 'Bairro das Indústrias I (Barreiro)', 'Maracanã', 'PA', '68710-000', 'residencial', '2025-12-05', '2025-12-05');
 
 -- --------------------------------------------------------
 
@@ -80,9 +81,9 @@ CREATE TABLE `estoque` (
 
 INSERT INTO `estoque` (`id`, `nome_item`, `descricao`, `quantidade`, `preco_custo`, `preco_venda`, `data_criacao`, `data_atualizacao`) VALUES
 (1, 'calça jeans preta', 'calça preta', 335, 35.00, 100.00, '2025-11-02 18:57:34', '2025-12-04 18:01:54'),
-(2, 'calça jeans preta', 'calça preta', 10, 35.00, 100.00, '2025-11-02 18:57:47', '2025-11-02 18:57:47'),
-(3, 'calça azul', 'calça azul', 665, 33.00, 66.00, '2025-11-02 18:58:11', '2025-12-04 18:03:51'),
-(4, 'calça bege', 'calça bege', 555, 66.00, 120.00, '2025-11-02 18:58:36', '2025-12-04 17:50:00'),
+(2, 'calça jeans preta', 'calça preta', 7, 35.00, 100.00, '2025-11-02 18:57:47', '2025-12-05 17:36:25'),
+(3, 'calça azul', 'calça azul', 627, 33.00, 66.00, '2025-11-02 18:58:11', '2025-12-05 17:37:12'),
+(4, 'calça bege', 'calça bege', 544, 66.00, 120.00, '2025-11-02 18:58:36', '2025-12-05 17:36:27'),
 (5, 'Boné', 'Boné aba reta', 0, 100.00, 140.00, '2025-11-14 19:52:22', '2025-12-04 18:04:00');
 
 -- --------------------------------------------------------
@@ -107,10 +108,12 @@ CREATE TABLE `log_estoque` (
 --
 
 INSERT INTO `log_estoque` (`id_log`, `id_item`, `quantidade_anterior`, `quantidade_nova`, `diferenca`, `tipo_movimentacao`, `usuario`, `data_hora`) VALUES
-(4, 3, 60, 65, '5', 'Entrada', 'desconhecido', '2025-12-04 18:01:49'),
-(5, 1, 30, 335, '305', 'Entrada', 'desconhecido', '2025-12-04 18:01:54'),
-(6, 3, 65, 665, '+600', 'Entrada', 'desconhecido', '2025-12-04 18:03:51'),
-(7, 5, 100, 0, '-100', 'Saída', 'desconhecido', '2025-12-04 18:04:00');
+(4, 3, 60, 65, '+5', 'Entrada', 'danton', '2025-12-04 18:01:49'),
+(5, 1, 30, 335, '+305', 'Entrada', 'danton', '2025-12-04 18:01:54'),
+(6, 3, 65, 665, '+600', 'Entrada', 'danton', '2025-12-04 18:03:51'),
+(7, 5, 100, 0, '-100', 'Saída', 'danton', '2025-12-04 18:04:00'),
+(8, 2, 10, 20, '+10', 'Entrada', 'danton', '2025-12-05 17:25:52'),
+(9, 2, 20, 15, '-5', 'Saída', 'Marcus', '2025-12-05 17:29:54');
 
 -- --------------------------------------------------------
 
@@ -141,16 +144,18 @@ INSERT INTO `orcamento_estoque` (`id`, `idcliente`, `cliente`, `doc_cliente`, `d
 (8, 0, 'teste', NULL, '2025-11-02 19:46:52', 'maria', '543543', 1233.00, 0, '', 0, 'Cancelado'),
 (9, 0, 'danton', NULL, '2025-11-02 19:50:18', 'maria', 'sadas', 12312.00, 0, '', 0, 'Cancelado'),
 (10, 0, 'danton', NULL, '2025-11-14 17:09:10', 'dantinho', 'teste', 1232131.00, 1, '', 10, 'Cancelado'),
-(11, 0, 'danton', NULL, '2025-11-14 17:14:41', 'dantinho2', 'testttt', 231231.00, 4, 'calça bege', 24, 'Aprovado'),
+(11, 0, 'danton', NULL, '2025-11-14 17:14:41', 'dantinho2', 'testttt', 231.00, 4, 'calça bege', 24, 'Aprovado'),
 (12, 0, 'Marcus', NULL, '2025-11-14 19:49:11', 'Fabio', 'Calça azul', 200.00, 3, 'calça azul', 2, 'Aprovado'),
 (13, 0, 'Marcus', NULL, '2025-11-14 19:50:41', 'Fabio', 'vdgeeb', 200.00, 1, 'calça jeans preta', 7, 'Aprovado'),
 (14, 0, 'leo', NULL, '2025-11-25 08:14:37', 'roney', 'venda de dois bones', 160.00, 5, 'Boné', 2, 'Aprovado'),
-(15, 0, 'leo', NULL, '2025-11-28 21:37:22', 'roney', '.', 160.00, 5, 'Boné', 1, 'Pendente'),
-(16, 0, '4', '1345679089', '2025-12-05 12:36:45', 'Marcus', '18x calças', 12111.00, 3, 'calça azul', 18, 'Pendente'),
-(17, 0, '1', '111111111', '2025-12-05 12:48:56', 'Marcus', 'test', 232.00, 3, 'calça azul', 15, 'Pendente'),
-(18, 4, 'Guilherme', '1345679089', '2025-12-05 12:53:30', 'Marcus', 'teste', 33.00, 4, 'calça bege', 9, 'Pendente'),
-(19, 1, 'Marcus', '111111111', '2025-12-05 12:54:06', 'Marcus', 'teste', 333.00, 2, 'calça jeans preta', 8, 'Pendente'),
-(20, 4, 'Guilherme', '1345679089', '2025-12-05 14:22:02', 'Marcus', 'teste', 240.00, 4, 'calça bege', 2, 'Pendente');
+(15, 0, 'leo', NULL, '2025-11-28 21:37:22', 'roney', '.', 160.00, 5, 'Boné', 1, 'Cancelado'),
+(16, 0, '4', '1345679089', '2025-12-05 12:36:45', 'Marcus', '18x calças', 12111.00, 3, 'calça azul', 18, 'Aprovado'),
+(17, 0, '1', '111111111', '2025-08-05 12:48:56', 'Marcus', 'test', 15000.00, 3, 'calça azul', 15, 'Aprovado'),
+(18, 4, 'Guilherme', '1345679089', '2025-12-05 12:53:30', 'Marcus', 'teste', 33.00, 4, 'calça bege', 9, 'Aprovado'),
+(19, 1, 'Marcus', '111111111', '2025-12-05 12:54:06', 'Marcus', 'teste', 333.00, 2, 'calça jeans preta', 8, 'Aprovado'),
+(20, 4, 'Guilherme', '1345679089', '2025-05-05 14:22:02', 'Marcus', 'teste', 10000.00, 4, 'calça bege', 2, 'Aprovado'),
+(21, 1, 'Marcus', '111111111', '2025-10-05 17:37:07', 'Marcus', '5x calça azul', 5.00, 3, 'calça azul', 5, 'Aprovado'),
+(22, 4, 'Guilherme', '1345679089', '2025-12-05 19:31:01', 'Marcus', 'gggg', 132.00, 3, 'calça azul', 2, 'Pendente');
 
 -- --------------------------------------------------------
 
@@ -184,7 +189,12 @@ INSERT INTO `payments` (`id`, `plan_id`, `plan_name`, `billing_cycle`, `amount`,
 (5, 8, 'Trimestral', 'monthly', 249.00, 'boleto', 'Lucas Augusto Medeiros Ramos', 'diegotrakinao@gmail.com', 'pending', '212E2EC21A79', '2025-11-13 04:35:18'),
 (6, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Lucas Augusto Medeiros Ramos', 'diegotrakinao@gmail.com', 'approved', 'cs_test_a1qHAAQmH4KaMdm2AFUwVxMNFMq5bJvN3DsrIezQEmXh9UbFdCAH5YHaOs', '2025-11-28 18:13:24'),
 (7, 7, 'Mensal', 'monthly', 89.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'rejected', 'FD0764195543', '2025-11-30 20:44:03'),
-(8, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'rejected', '4B1FB6863A1B', '2025-11-30 21:24:16');
+(8, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'rejected', '4B1FB6863A1B', '2025-11-30 21:24:16'),
+(9, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'pending', 'cs_test_a1TNDX7Wpq0buPuq2x4BXcwBSySMQvca5MC1uvNEu038ptrsfPQcXseOFK', '2025-12-05 22:19:27'),
+(10, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'pending', 'cs_test_a1sOpHZfcXAGo9hcSTx5VBRj0cRss5elzXoskYfCUkqheWeBy7g3lSsCzx', '2025-12-05 22:36:30'),
+(11, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'pending', 'cs_test_a1PYhrvtO5zn3USxtehwmAitwZQAeA5nSqQylP3YJGVxa9kW4hHR1F6he2', '2025-12-07 22:05:47'),
+(12, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'pending', 'cs_test_a1zCPHAzztBBos7muy8fCKAtKfZ2oCUFnslf1MR5G0TA7mDGSLwXNhVGEs', '2025-12-07 22:12:02'),
+(13, 8, 'Trimestral', 'monthly', 249.00, 'card', 'Marcus', 'stmarcusandrade44@gmail.com', 'pending', 'cs_test_a1mwIMRHmjsSr6awSH88JKYLhLGrJTo7rW9ySD6WhgqgHYG08ShhQxNZWx', '2025-12-07 22:17:23');
 
 -- --------------------------------------------------------
 
@@ -228,23 +238,25 @@ CREATE TABLE `usuarios` (
   `tipo_usuario` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `data_cadastro` datetime NOT NULL,
-  `data_atualizacao` datetime NOT NULL,
-  `status` int(1) NOT NULL
+  `must_change_password` tinyint(1) NOT NULL DEFAULT 0,
+  `data_cadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `data_atualizacao` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `usuario`, `tipo_usuario`, `senha`, `email`, `data_cadastro`, `data_atualizacao`, `status`) VALUES
-(1, 'Marcus', 'MarcusV', 'gerente', 'M@rcusone55', 'marcus44@ac.com', '2025-11-02 19:55:32', '2025-11-27 01:56:08', 1),
-(5, 'Vinicius', 'Vini', 'gerente', 'gucelo08', 'vnmz@gmail.com', '2025-11-21 18:30:40', '2025-11-21 18:30:40', 1),
-(6, 'Andrade', 'addd', 'gerente', 'gucelo08', 'stmarcusandrade44@gmail.com', '2025-11-21 18:33:19', '2025-11-21 18:33:19', 1),
-(7, 'Ryan', 'Terril', 'gerente_estoque', 'gucelo08', 'plenoR@gmail.com', '2025-11-21 18:47:15', '2025-11-21 18:47:15', 1),
-(8, 'AAA', 'senai', 'vendedor', 'gucelo08', 'frg@hh.com', '2025-11-21 20:02:43', '2025-11-21 22:02:04', 1),
-(9, 'Gleison', 'pucprof@gmail.com', 'vendedor', 'hugo', 'vnmz@gmail.com', '2025-11-21 21:56:30', '2025-11-21 21:56:30', 1),
-(10, 'Roney', 'Roneyvon', 'vendedor', 'gucelo08', 'roney@gmail.com', '2025-11-25 12:12:38', '2025-11-27 02:26:48', 0);
+INSERT INTO `usuarios` (`id`, `nome`, `usuario`, `tipo_usuario`, `senha`, `email`, `must_change_password`, `data_cadastro`, `data_atualizacao`, `status`) VALUES
+(1, 'Marcus', 'MarcusV', 'gerente', 'M@rcusone55', 'marcus44@ac.com', 0, '2025-11-02 19:55:32', '2025-11-21 23:32:47', 1),
+(5, 'Vinicius', 'Vini', 'gerente', 'gucelo08', 'vnmz@gmail.com', 0, '2025-11-21 18:30:40', '2025-11-21 18:30:40', 1),
+(6, 'Andrade', 'addd', 'gerente', '$2y$10$RPeXvsaOx0xwdw8lRdfPZebQWsWRXlfqq969t.uuQwyU0b9/hP44W', 'stmarcusandrade44@gmail.com', 1, '2025-11-21 18:33:19', '2025-12-07 19:24:51', 1),
+(7, 'Ryan', 'Terril', 'gerente_estoque', 'gucelo08', 'plenoR@gmail.com', 0, '2025-11-21 18:47:15', '2025-11-21 18:47:15', 1),
+(8, 'AAA', 'senai', 'gerente', 'gucelo08', 'frg@hh.com', 0, '2025-11-21 20:02:43', '2025-11-21 22:02:04', 1),
+(9, 'Gleison', 'pucprof@gmail.com', 'gerente', 'hugo', 'gleison.prof@empresa.com', 0, '2025-11-21 21:56:30', '2025-11-21 21:56:30', 1),
+(10, 'Lucas Ramos', 'diegotrakinao', 'cliente', '$2y$10$vUvYSMo6ziET66WcLKcONu23cWUZ02ABKLTDK9wcp24ICuCLSfTC6', 'diegotrakinao@gmail.com', 1, '2025-11-28 15:13:59', '2025-12-05 16:00:54', 1),
+(11, 'Lucas Ramos', 'flashdounder', 'cliente', '$2y$10$pd3hPjhDCmiA4zurHvsW6O73y8EdcL5g81VbNeSZhB0LPT9ws9KJK', 'flashdounder@gmail.com', 1, '2025-12-04 12:15:28', '2025-12-04 12:48:48', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -290,7 +302,8 @@ ALTER TABLE `plans`
 -- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uniq_usuarios_email` (`email`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -300,7 +313,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `estoque`
@@ -312,19 +325,19 @@ ALTER TABLE `estoque`
 -- AUTO_INCREMENT de tabela `log_estoque`
 --
 ALTER TABLE `log_estoque`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `orcamento_estoque`
 --
 ALTER TABLE `orcamento_estoque`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de tabela `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de tabela `plans`
@@ -336,7 +349,7 @@ ALTER TABLE `plans`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
